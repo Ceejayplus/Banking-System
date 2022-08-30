93b6f28836e2f5d76de85579fb31cd347a55a5b5
Goodness <babasolagoodness21@gmail.com>
2022-08-30 10:35:39 +0100

    I gave the id of the input tags a variable in javascript

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 customerDetails = []
 const signUp = () => {
     userDetails = {
-        
+        firstName : firstname.value,
+        lastName : lastname.value,
+        Email : useremail.value,
+        phonenumber : userphonenumber.value,
+        password : userpassword.value
     }
 }
