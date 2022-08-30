22243e667b61de10c511774600dd3292950c4e76
Goodness <babasolagoodness21@gmail.com>
2022-08-30 10:49:15 +0100

    I am recieving the user inputs into the javascript array

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 customerDetails = []
+
+let accountNum =Math.round(Math.random()*1000000)
+// let userPin = Math.round(Math.random()*10000)
 const signUp = () => {
     userDetails = {
         firstName : firstname.value,
         lastName : lastname.value,
         Email : useremail.value,
         phonenumber : userphonenumber.value,
-        password : userpassword.value
+        password : userpassword.value,
+        accountNumber : accountNum
+    }
+
+    if((firstName = firstname.value) && (lastName = lastname.value) && (Eamil = useremail.value) && (phonenumber = userphonenumber.value) && (password = userpassword.value)){
+        customerDetails.push(userDetails)
+        firstname.value = ""
+        lastname.value = ""
+        useremail.value = ""
+        userphonenumber = ""
+        
     }
 }
